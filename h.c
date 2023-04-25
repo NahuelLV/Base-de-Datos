11044a20f9404b89788152a244ce459051b61cbf
NahuelLV <nahuellinaresviau@gmail.com>
2023-04-24 23:31:21 -0300

    arreglo Hola mundo

---
+++ h.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Hola Mundo, Como estan?");
+
+	return 0;
+
+}
+
+
+      
+
+
+
+
--- p.c
+++ p.c
@@ -4,7 +4,7 @@
 
 
 int main(void) {
-	printf("Hola MUndo, vida de mierda");
+	printf("Hola Mundo,Como estan?");
 
 	return 0;
 
